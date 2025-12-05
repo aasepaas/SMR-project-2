dcf0a6a7ba557a2ea7f8db456c7e6723c4ac5169
KangarooAteMe <bastiaan.wolters@proton.me>
2025-12-05 13:40:33 +0100

    updated Bastiaan Code

--- database/check.php
+++ database/check.php
@@ -1,11 +1,32 @@
 <?php
 require 'database.php';
 
-$vals = json_decode($argv[1], true);
-$boxid = $vals['boxid'];
-$id = $vals['id'];
-$db = new DatabaseConfig(DatabaseConfig::$servernaam, DatabaseConfig::$gebruikersnaam, DatabaseConfig::$wachtwoord, DatabaseConfig::$databasenaam);
-$result = $db->checkID($boxid, $id);
-echo $result;
-$db->closeConnection();
+$function = $argv[1] ?? null;
+$json_arg = $argv[2] ?? "{}";
+$vals = json_decode($json_arg, true);
+function check_boxid($vals)
+{
+
+    $boxid = $vals['boxid'];
+    $db = new DatabaseConfig("localhost", "appuser", "StrongPassword123!", "test");
+    $result = $db->check_box_id($boxid);
+    echo json_encode(["result" => $result]);
+    $db->closeConnection();
+}
+function final_check($vals)
+{
+
+    $boxid = $vals['boxid'];
+    $id = $vals['id'];
+    $db = new DatabaseConfig("localhost", "appuser", "StrongPassword123!", "test");
+    $result = $db->checkID($boxid, $id);
+    echo json_encode(["result" => $result]);
+    $db->closeConnection();
+}
+
+if (function_exists($function)) {
+    $function($vals);
+} else {
+    echo "Function does not exist";
+}
 ?>
--- database/rundatabase.py
+++ database/rundatabase.py
@@ -1,33 +1,135 @@
+
 import subprocess
 import json
+import time
+import threading
 
 
-payload = {
-    "boxid": "123",
-    "id": "456"
-}
+from scanner import CameraScanner, ScanProfile, DataMatrixDecoder
+from Statuscontrol import StatusControl
+from state_enum import state_enum
+
+class DatabaseRun:
+        def __init__(self):
+            boxid, ids = None, None
+            self.bbuffer, self.ibuffer = None, None
+            self.lock = threading.Lock()
+
+        def check_box_id(self, valswallet, valsprotector, dummyprof, decoder):
+            cam_thread = threading.Thread(target=dummyprof.run, args=(valswallet, valsprotector), daemon = True)
+            cam_thread.start()
+            while self.bbuffer is  None:
+                with self.lock:
+                    if decoder.last_code is not None:
+                        boxid, ids = get_values(valswallet, valsprotector, dummyprof, self.iterator)
+                        self.iterator += 1
+                    if boxid is not None:
+                        self.bbuffer = boxid
+                    print(f"Box ID: {self.bbuffer}")
+            dummyprof.stop()
+            cam_thread.join()
+            self.send_data(state_enum.SCANNING_GIFTBOX)
+
+        def collect_codes(self, valswallet, valsprotector, dummyprof, decoder):
+            cam_thread = threading.Thread(target=dummyprof.run, args=(valswallet, valsprotector), daemon = True)
+            cam_thread.start()
+
+            while self.bbuffer is  None or self.ibuffer is  None:
+                with self.lock:
+                    if decoder.last_code is not None:
+                        boxid, ids = get_values(valswallet, valsprotector, dummyprof)
+                        
+
+                    if boxid is not None:
+                        self.bbuffer = boxid
+                    if ids is not None:
+                        self.ibuffer = ids
+                print(f"Box ID: {self.bbuffer}, IDs: {self.ibuffer}")
+
+            dummyprof.stop()
+            cam_thread.join()
 
 
-json_arg = json.dumps(payload)
+        def send_data(self, state):
 
-process = subprocess.Popen(
-    ["php", "check.php", json_arg],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    text=True
-)
+            if state == state_enum.SCANNING_WALLET:
+            
 
-stdout, stderr = process.communicate()
+        
+                payload = {
+                            "boxid": self.bbuffer,
+                            "id": self.ibuffer
+                        }
 
-if stderr:
-    print("PHP Error:", stderr)
 
-try:
-    result = json.loads(stdout)
-    print("Result from PHP:", result)
-except json.JSONDecodeError:
-    print("Invalid JSON received:")
-    print(stdout)
+                json_arg = json.dumps(payload)
+
+                process = subprocess.Popen(
+                            [r"C:\xampp\php\php.exe", r"C:\Users\basti\source\repos\databaserun\check2.php", "final_check", json_arg],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            text=True
+                        )
+                self.bbuffer, self.ibuffer = None, None
+
+                
+
+            elif state == state_enum.SCANNING_GIFTBOX:
+                payload = {
+                            "boxid": self.bbuffer
+                        }
+                json_arg = json.dumps(payload)
+                process = subprocess.Popen(
+                            [r"C:\xampp\php\php.exe", r"C:\Users\basti\source\repos\databaserun\check2.php","check_boxid", json_arg],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            text=True
+                        )
+                self.bbuffer = None
+
+            stdout, stderr = process.communicate()
+
+            print("=== RAW STDOUT ===")
+            print(repr(stdout))
+            print("=== RAW STDERR ===")
+            print(repr(stderr))
+
+
+            if stderr:
+                print("PHP Error:", stderr)
+
+            try:
+                        result = json.loads(stdout)
+                        print(result)
+                        print("Result from PHP:", result)
+            except json.JSONDecodeError:
+                        print("Invalid JSON received:")
+                        print(stdout)
+
+                
+
+
+                
+             
+
+
+def get_values(valswallet, valsprotector, dummy):
+    with dummy.lock:
+        state = StatusControl().get_status()
+
+    if state == state_enum.SCANNING_GIFTBOX:
+        return valswallet.get_code(), None
+
+    if state == state_enum.SCANNING_WALLET:
+        return None, valsprotector.get_code()
+
+    return None, None
+
+
+
+
+       
+
 
 
 
